dcc29810b6e71ce811a35b18a779ded5cba8d419
SaurabhMagara <saurabhmagara@gmail.com>
2025-01-12 13:30:56 +0530

    navbar added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.vecteezy.com',
+        port: '',
+        pathname: '/system/resources/previews/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
