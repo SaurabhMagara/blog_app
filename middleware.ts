3d063ef60701b4e05ea9231ce52b514ef2170f7f
SaurabhMagara <saurabhmagara@gmail.com>
2025-01-28 13:35:41 +0530

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,14 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import {jwtVerify} from "jose";
 
 export async function middleware(request: NextRequest) {
     try {
         // Extract the token from cookies
         const token = request.cookies.get("token")?.value;
 
-        console.log("Token from cookies:", token); // Debugging
+        // Debugging
+        console.log("Token from cookies:", token);
 
         // If no token is found, respond with 401 Unauthorized
         if (!token) {
@@ -16,12 +18,26 @@ export async function middleware(request: NextRequest) {
             );
         }
 
-        // If the token exists, allow the request to proceed
+        // Verify the token
+        const secret = new TextEncoder().encode(process.env.JWT_SECRET); // Convert the secret to Uint8Array
+        const { payload } = await jwtVerify(token, secret); // Decode and verify the token
+        console.log("Decoded token:", payload);
+
+        // If the token is valid, proceed with the request
         return NextResponse.next();
     } catch (error: any) {
-        console.error("Middleware error:", error.message);
+        console.error("Middleware error:", error);
+
+        // Handle token verification errors
+        if (error.name === "JsonWebTokenError") {
+            return NextResponse.json(
+                { message: "Invalid token." },
+                { status: 401 }
+            );
+        }
+
         return NextResponse.json(
-            { error: "Internal server error" },
+            { message: "Internal server error.", error : error },
             { status: 500 }
         );
     }
@@ -31,6 +47,6 @@ export async function middleware(request: NextRequest) {
 export const config = {
     matcher: [
         "/api/v1/logout",
-        "/api/v1/users/:userid",
+        "/api/v1/users/:userid*",
     ],
 };
